2a2114dcfedc1a88cc48ff35d87727b3f28a9d14
mickeahlinder <micke.ahlinder@gmail.com>
2023-01-23 18:48:12 +0100

    remove debugging info

--- src/tfenv/install.sh
+++ src/tfenv/install.sh
@@ -4,12 +4,6 @@ VERSION=${VERSION:-undefined}
 
 set -e
 
-echo "The effective dev container remoteUser is '$_REMOTE_USER'"
-echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
-
-echo "The effective dev container containerUser is '$_CONTAINER_USER'"
-echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
-
 # Clean up
 rm -rf /var/lib/apt/lists/*
 
